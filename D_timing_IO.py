c66bc36b71ef8bb36389a7eea5146a6a6601f5f6
Peerasu <fecownp@gmail.com>
2023-08-09 01:50:11 +0700

    add time-to-call-recursive for better comparison

--- D_timing_IO.py
+++ D_timing_IO.py
@@ -1,24 +1,36 @@
 import time
 
-start=0; end=0; time_input=0; time_output=0; time_for_loop=0
+def recursive(num):
+    if num == 0:
+        return 0
+    return recursive(num-1)
+
+start=0; end=0; time_input=0; time_output=0; time_for_loop=0; time_recursive=0
 
 start = time.perf_counter()
-input("input: ")
+input = input("receive input: ")
 end = time.perf_counter()
 time_input = (end-start)*pow(10, 6)
 
 start = time.perf_counter()
-print("test output time")
+print(f"show output: {input}")
 end = time.perf_counter()
 time_output = (end-start)*pow(10, 6)
 
 start = time.perf_counter()
-for i in range(10000):
+for i in range(1000):
     pass
 end = time.perf_counter()
 time_for_loop = (end-start)*pow(10, 6)
 
+start = time.perf_counter()
+recursive(500)
+end = time.perf_counter()
+time_recursive = (end-start)*pow(10, 6)
+
+print()
 print(f"time to receive input: {time_input} microseconds")
 print(f"time to show output: {time_output} microseconds")
-print(f"time to do for loop 10000 times: {time_for_loop} microseconds")
+print(f"time to do for loop 1000 times: {time_for_loop} microseconds")
+print(f"time to call recursive function 500 times: {time_recursive} microseconds")
 
