eb06847dbe900665e573e908c7458820da51c427
Peerasu <fecownp@gmail.com>
2023-08-09 02:20:02 +0700

    remove time_input, change time_output to time_IO

--- D_timing_IO.py
+++ D_timing_IO.py
@@ -5,17 +5,12 @@ def recursive(num):
         return 0
     return recursive(num-1)
 
-start=0; end=0; time_input=0; time_output=0; time_for_loop=0; time_recursive=0
+start=0; end=0; time_IO=0; time_for_loop=0; time_recursive=0
 
 start = time.perf_counter()
-input = input("receive input: ")
+print("test I/O time")
 end = time.perf_counter()
-time_input = (end-start)*pow(10, 6)
-
-start = time.perf_counter()
-print(f"show output: {input}")
-end = time.perf_counter()
-time_output = (end-start)*pow(10, 6)
+time_IO = (end-start)*pow(10, 6)
 
 start = time.perf_counter()
 for i in range(1000):
@@ -28,9 +23,7 @@ recursive(500)
 end = time.perf_counter()
 time_recursive = (end-start)*pow(10, 6)
 
-print()
-print(f"time to receive input: {time_input} microseconds")
-print(f"time to show output: {time_output} microseconds")
+print(f"time for I/O: {time_IO} microseconds")
 print(f"time to do for loop 1000 times: {time_for_loop} microseconds")
 print(f"time to call recursive function 500 times: {time_recursive} microseconds")
 
