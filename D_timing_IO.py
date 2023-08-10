b1a42d8ec32e36b200fc0b1ee9460acc371d3fa8
Peerasu <fecownp@gmail.com>
2023-08-10 11:28:13 +0700

    comment to explain how to deal with "test_IO.txt"

--- D_timing_IO.py
+++ D_timing_IO.py
@@ -1,3 +1,11 @@
+# make sure that you already have file "test_IO.txt" in your path.
+
+# if you don't have the file "test_IO.txt" in your path.
+# uncomment and run this code ONLY in the first time.
+
+# f = open("test_IO.txt", "x")
+# f.write("Peerasu")
+
 import time
 
 def recursive(num):
@@ -6,10 +14,6 @@ def recursive(num):
     return recursive(num-1)
 
 start=0; end=0; time_input=0; time_output=0; time_for_loop=0; time_recursive=0
-
-# run this code only in the first time
-# f = open("test_IO.txt", "x")
-# f.write("Peerasu")
 
 start = time.perf_counter()
 text = open("test_IO.txt", "r")
