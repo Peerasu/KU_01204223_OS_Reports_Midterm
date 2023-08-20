c21d7b35d9df771149835003d2c084ddd2d702d5
Peerasu <fecownp@gmail.com>
2023-08-20 16:25:59 +0700

    use os libraly to remove file after program end

--- D_timing_IO.py
+++ D_timing_IO.py
@@ -1,18 +1,13 @@
-# make sure that you already have file "test_IO.txt" in your path.
-
-# if you don't have the file "test_IO.txt" in your path.
-# uncomment and run this code ONLY in the first time.
-
-# f = open("test_IO.txt", "x")
-# f.write("Peerasu")
-
 import time
+import os
 
 def recursive(num):
     if num == 0:
         return 0
     return recursive(num-1)
 
+f = open("test_IO.txt", "x")
+f.write("Peerasu")
 
 start=0; end=0; time_input=0; time_output=0; time_for_loop=0; time_recursive=0
 
@@ -41,10 +36,13 @@ recursive(100)
 end = time.perf_counter()
 time_recursive = (end-start)*pow(10, 6)
 
+print("\nRESULT\n")
 print(f"time for input: {time_input} microseconds")
 print(f"time for output: {time_output} microseconds")
 print(f"time to do for loop 100 times: {time_for_loop} microseconds")
-print(f"time to call recursive function 100 times: {time_recursive} microseconds")
+print(f"time to call recursive function 100 times: {time_recursive} microseconds\n")
 
+f.close()
 text.close()
+os.remove("test_IO.txt")
 
